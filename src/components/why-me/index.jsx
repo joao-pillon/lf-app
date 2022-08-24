215c2f5ba54867665fe294bac23cdfbaf3c2dc1d
Jp <Joo Pedro Ferreira Pillon>
2022-08-24 16:42:03 -0300

    feat: prices

--- src/components/why-me/index.jsx
+++ src/components/why-me/index.jsx
@@ -1,11 +1,29 @@
 import "./index.css";
 
 const WhyMe = () => {
-  return <section id="why-me">
-    <div className="container">
-<h2>Por quê eu sou a pessoa certa para te ensinar</h2>
-<h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, tenetur. Quidem fuga quaerat vitae rerum reprehenderit veritatis eveniet. Distinctio, incidunt minima! Molestiae fugit repudiandae tempore eveniet praesentium nobis omnis hic.</h5>
-</div></section>;
+  return (
+    <section id="why-me">
+      <div className="container">
+        <h2>Por quê eu sou a pessoa certa para te ensinar</h2>
+        <h5>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+          tenetur. Quidem fuga quaerat vitae rerum reprehenderit veritatis
+          eveniet. Distinctio, incidunt minima! Molestiae fugit repudiandae
+          tempore eveniet praesentium nobis omnis hic.
+        </h5>
+      </div>
+
+      <div className="price">
+        <h2>Preços</h2>
+        <h5>XXXXXXXX</h5>
+        <h5>XXXXXXXX</h5>
+        <h5>XXXXXXXX</h5>
+        <h5>XXXXXXXX</h5>
+        <h5>XXXXXXXX</h5>
+        
+      </div>
+    </section>
+  );
 };
 
 export default WhyMe;
